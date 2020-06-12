b0c0ea44ff108f5067c4a0c3059206a9dfc2ea32
Alexander Ursu <alexander.ursu@gmail.com>
2020-06-13 00:55:18 +0200

    Corrected filter statement

--- functions/selfaddr.pp
+++ functions/selfaddr.pp
@@ -1,5 +1,5 @@
 function grayloginstall::selfaddr(Stdlib::IP::Address $addr) >> Boolean {
-  $interfaces = $::facts['networking']['interfaces'].filter |$iface| { $iface[1]['ip'] }
+  $interfaces = $::facts['networking']['interfaces'].filter |$iface| { $iface[1]['ip'] =~ Stdlib::IP::Address }
 
   $ip = $interfaces.map |$iface, $data| { $data['ip'] }
   $ip6 = $interfaces.map |$iface, $data| { $data['ip6'] }
