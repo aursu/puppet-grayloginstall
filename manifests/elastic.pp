3f993dbb245de96448cf7e35220968c7e88326f6
Alexander Ursu <alexander.ursu@gmail.com>
2020-06-14 22:11:04 +0200

    Added puppet strings definitions

--- manifests/elastic.pp
+++ manifests/elastic.pp
@@ -1,18 +1,40 @@
-# @summary A short summary of the purpose of this class
+# @summary Configure elasticsearch for use with Graylog
 #
-# A description of what this class does
+# @example Basic usage
+#   class { 'grayloginstall::elastic':
+#     network_host         => '_eth1:ipv4_',
+#     discovery_seed_hosts => ['192.168.178.10', '192.168.178.20'],
+#   }
 #
-# @example
-#   include grayloginstall::elastic
+# @see https://docs.graylog.org/en/3.3/pages/installation/os/centos.html#elasticsearch
+#
+# @param version
+#   Elasticsearch version (Graylog 3 does not work with Elasticsearch 7.x)
+#
+# @param network_host
+#   The node will bind to this hostname or IP address and publish (advertise) this host to other nodes in the cluster. Accepts
+#   an IP address, hostname, a special value.
+#   Default is _site_ (any site-local addresses on the system, for example 192.168.0.1)
+#   See https://www.elastic.co/guide/en/elasticsearch/reference/6.8/modules-network.html#network-interface-values
+#
+# @param minimum_master_nodes
+#   it is vital to configure the discovery.zen.minimum_master_nodes setting so that each master-eligible node knows the minimum
+#   number of master-eligible nodes that must be visible in order to form a cluster.
+#   Default is 2 (avoiding split brain in case of 2-3 nodes)
+#   See https://www.elastic.co/guide/en/elasticsearch/reference/6.8/discovery-settings.html#minimum_master_nodes
+#
+# @param discovery_seed_hosts
+#   seed list of other nodes in the cluster.
+#   Default is ["127.0.0.1", "[::1]"] - elasticsearch will bind to the available loopback addresses and will scan ports 9300
+#   to 9305 to try to connect to other nodes running on the same server
+#   See https://www.elastic.co/guide/en/elasticsearch/reference/6.8/discovery-settings.html#unicast.hosts
+#
 class grayloginstall::elastic (
-  # Graylog 3 does not work with Elasticsearch 7.x!
   String  $version              = '6.8.10',
-  # Any site-local addresses on the system, for example 192.168.0.1
   Grayloginstall::NetworkHost
           $network_host         = '_site_',
-  # default is 2 (avoiding split brain in case of 2-3 nodes)
   Integer $minimum_master_nodes = 2,
-  # default is ["127.0.0.1", "[::1]"]
+
   Array[Stdlib::IP::Address]
           $discovery_seed_hosts = ['127.0.0.1', '::1'],
 )
