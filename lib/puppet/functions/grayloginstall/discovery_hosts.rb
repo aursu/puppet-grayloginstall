0171293fb76d4f8bc50a664630766bc4c14c26ab
Alexander Ursu <alexander.ursu@gmail.com>
2020-06-15 05:53:09 +0200

    Reworked based on Puppet::Pops::Evaluator::Collectors::ExportedCollector code

--- lib/puppet/functions/grayloginstall/discovery_hosts.rb
+++ lib/puppet/functions/grayloginstall/discovery_hosts.rb
@@ -1,17 +1,45 @@
 Puppet::Functions.create_function(:'grayloginstall::discovery_hosts', Puppet::Functions::InternalFunction) do
-  dispatch :discovery_hosts do
+  dispatch :discovery_hosts_title do
     scope_param
     param 'String', :lookup_type
-    optional_param 'String', :lookup_param
+    optional_param 'Array', :equery
   end
 
-  def discovery_hosts(scope, lookup_type, lookup_param = nil)
-    resources = scope.compiler.resources.select { |r| r.type == lookup_type }.reject { |r| r.exported? }
-    if lookup_param.nil?
-      resources.map { |r| r.title.to_s }
-    else
-      p = lookup_param.to_sym
-      resources.reject { |r| r[p].nil? }.map { |r| r[p].to_s }
+  dispatch :discovery_hosts_param do
+    scope_param
+    param 'String', :lookup_type
+    param 'String', :lookup_param
+    optional_param 'Array', :equery
+  end
+
+  def exported_collector_collect(scope, lookup_type, equery = nil)
+    resources = scope.compiler.resources.select { |r| r.type == lookup_type && r.exported? }
+
+    found = Puppet::Resource.indirection
+                            .search(lookup_type, host: scope.compiler.node.name, filter: equery, scope: scope)
+
+    found_resources = found.map { |x| x.is_a?(Puppet::Parser::Resource) ? x : x.to_resource(scope) }
+
+    found_resources.each do |item|
+      unless scope.findresource(item.resource_type, item.title)
+        resources << item
+      end
     end
+
+    resources
+  end
+
+  # param [Array] equery an array representation of the query (exported query)
+  def discovery_hosts_title(scope, lookup_type, equery = nil)
+    resources = exported_collector_collect(scope, lookup_type, equery)
+    resources.map { |r| r.title.to_s }
+  end
+
+  # param [Array] equery an array representation of the query (exported query)
+  def discovery_hosts_param(scope, lookup_type, lookup_param, equery = nil)
+    resources = exported_collector_collect(scope, lookup_type, equery)
+
+    p = lookup_param.to_sym
+    resources.reject { |r| r[p].nil? }.map { |r| r[p].to_s }
   end
 end
