55dd7ce3d658ecc6a2b24128c4a40cab68a3f163
Alexander Ursu <alexander.ursu@gmail.com>
2020-07-02 14:57:40 +0200

    Upgrade default to 3.3.2

--- manifests/params.pp
+++ manifests/params.pp
@@ -18,7 +18,7 @@ class grayloginstall::params {
   $mongodb_database = 'graylog'
   $mongodb_user = 'graylog'
 
-  $graylog_version = '3.3.1'
+  $graylog_version = '3.3.2'
 
   $rel = split($graylog_version, '[.]')
   $graylog_major = join($rel[0,2], '.')
